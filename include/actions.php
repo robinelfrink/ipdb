f6ce432d84fa823e639fa1d335a176ae77a359f2
Robin Elfrink <robin@15augustus.nl>
2014-10-24 11:19:09 +0200

    Show new node after change.

--- include/actions.php
+++ include/actions.php
@@ -96,6 +96,7 @@ function acton($action) {
 								  break;
 							  }
 					  if (!$error) {
+						  request('node', request('address').'/'.request('bits'), true);
 						  request('page', 'main', true);
 					  }
 				  }
