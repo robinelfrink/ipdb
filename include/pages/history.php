31c425a2f8debeaad3229f3c1bd3e70132b284e2
Robin Elfrink <robin@15augustus.nl>
2015-04-13 16:26:18 +0200

    Rewrite history navigation.

--- include/pages/history.php
+++ include/pages/history.php
@@ -34,27 +34,35 @@ class history {
 		$history = $database->getLog(request('historysearch'));
 
 		$total = count($history);
-		$start = request('pagenr', 1)-1;
-		$max = 20;
-		if ($total<($start*$max)) {
-			$start = 0;
+		$pagenr = request('pagenr', 1);
+		$maxperpage = 20;
+		$totalpages = floor($total/$maxperpage)+1;
+		if ($total<(($pagenr-1)*$maxperpage)) {
+			$pagenr = 1;
 			request('pagenr', 1, true);
 		}
-		$history = array_slice($history, $start*$max, $max);
 
-		if (($total/$max)<=1) {
+		if (($total/$maxperpage)<=1) {
 			$navigation = '';
 		} else {
 			$navigation = 'Jump to page ';
-			for ($p = 1; ($p-1)<($total/$max); $p++)
-				if (($p==1) ||
-					(abs($start+1-$p)<4) ||
-					(($p-1)==floor($total/$max)) ||
-					(($p % (floor($total/$max)/10))==0))
-					$navigation .= ($start==($p-1) ? $p : '<a href="'.me().'?pagenr='.$p.'">'.$p.'</a>').'&nbsp;';
-				else
+			$history = array_slice($history, ($pagenr-1)*$maxperpage, $maxperpage);
+			/* Build array of page numbers to show in navigation */
+			$pages = array_unique(array_merge(
+				range(1, min(3, $pagenr+3)), // first three pages
+				range(max(1, $pagenr-3), min($pagenr+3, $totalpages)), // current page +/- 3
+				range(max($start, $totalpages-3), $totalpages) // last three pages
+			), SORT_NUMERIC);
+			$previouspage = 1;
+			foreach ($pages as $page) {
+				if ($page>($previouspage+1))
 					$navigation .= '&hellip;&nbsp;';
-			$navigation = preg_replace('/(&hellip;&nbsp;)+/', '&hellip;&nbsp;', $navigation);
+				if ($page==$pagenr)
+					$navigation .= '<b>'.$page.'</b>&nbsp;';
+				else
+					$navigation .= '<a href="'.me().'?pagenr='.$page.'">'.$page.'</a>&nbsp;';
+				$previouspage++;
+			}
 		}
 
 		$even = true;
